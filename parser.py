208b723b4b236e43fbfd119a5a2cf4ec59b7d0e7
Alex Gaynor <alex.gaynor@gmail.com>
2009-11-19 21:35:16 -0500

    Started working to nondeterminism

--- parser.py
+++ parser.py
@@ -1,22 +1,38 @@
 from collections import deque
+from copy import copy
 
 
 class Parser(object):
     def __init__(self, tokens):
         self.tokens = deque(tokens)
-    
-    @property
-    def token(self):
-        return self.tokens[0]
+        self.states = set()
     
     def parse(self):
+        self.states.add((self.main_parse, self.tokens))
+        # While there's more than one next state, or if there's a single state
+        # that state has tokens
+        while len(self.states) > 1 and not iter(self.states).next()[1]:
+            next_state, tokens = self.states.pop()
+            next_state(tokens)
+        state = self.states.pop()
+        return state
+    
+    def main_parse(self, tokens):
         nodes = []
-        while self.tokens:
-            if self.token.name == "newline":
-                self.tokens.popleft()
+        while tokens:
+            if tokens[0].name == "newline":
+                tokens.popleft()
             else:
-                nodes.append(self.parse_statement())
+                nodes.append(self.parse_statement(), copy(tokens))
         return NodeList(nodes)
     
-    def parse_statement(self):
-        pass
+    def parse_statement(self, tokens):
+        self.states.add((self.simple_statement, copy(tokens)))
+        self.states.add((self.compound_statement, copy(tokens))
+    
+    def simple_statement(self, tokens):
+        self.states.add((self.expression, copy(tokens)))
+        self.states.add((self.decleration, copy(tokens))
+        self.states.add((self.assignment_statement, copy(tokens))
+        self.states.add((self.flow_statement, copy(tokens))
+        self.states.add((self.import_statement, copy(tokens))
